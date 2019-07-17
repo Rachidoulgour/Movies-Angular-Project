528f840959d84c44a1d346e9de92f48141132166
rachid <olgor@hotmail.com>
2019-07-17 10:17:47 +0200

    signup added

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,6 +2,25 @@ const router = require('express').Router;
 const userModel = require('../models/user');
 //const isAutenticated
 
+router.post('/signup', async(req,res)=>{
+    try{
+        const user = await new userModel({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        }).save()
+        const token = await user.generateAuthToken()
+        res.status(201).send({
+            info: "User succesfully created",
+            user,
+            token
+        })
+    }catch(error){
+        console.log(error)
+        res.status(400).send(error)
+    }
+})
+
 router.get('/all', async(req,res)=>{
     try{
         const users= await userModel.find({})
